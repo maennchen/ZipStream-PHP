7e4adae3bba32498abd716fadf2d7a2f53d1cea6
Jonatan Männchen <jonatan@maennchen.ch>
2014-11-15 12:16:23 +0000

    Move everything to the right place

--- tests/ZipStreamTest.php
+++ tests/ZipStreamTest.php
@@ -1,9 +1,9 @@
 <?php
-namespace ZipStream;
+namespace ZipStreamTest;
 
 use \PHPUnit_Framework_TestCase;
 
-class MainTest extends PHPUnit_Framework_TestCase {
+class ZipStreamTest extends PHPUnit_Framework_TestCase {
 	public function testXy() {
 		$this->assertEquals(1, 1);
 	}
