6e0c2995fa44e397984d2b37371848a55222acca
Nicolas CARPi <nico-git@deltablot.email>
2020-05-30 01:08:30 +0200

    Changelog and fix two psalm issues

--- src/Option/Version.php
+++ src/Option/Version.php
@@ -12,6 +12,7 @@ use MyCLabs\Enum\Enum;
  * @method static STORE(): Version
  * @method static DEFLATE(): Version
  * @method static ZIP64(): Version
+ * @psalm-immutable
  */
 class Version extends Enum
 {
